f63aba2387dfcb06a2ddb77ae6898022c267c8a5
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-19 15:17:53 +0300

    Projenin son hali

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int sayi=10;
+    double sayi1=12.5;
+    byte sayi3=125;
+    char karakter='a';
+    String sehir="Ankara";
+    boolean dogruMu=true;
+
     }
 }
